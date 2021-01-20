047b43f3d08b00ab12f6545e0a1fa83bc5e19e4a
Simona Cinti <simona.cinti2@gmail.com>
2021-01-20 16:11:04 +0100

    print json by vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
     <h1>Lorem ipsum</h1>
+    <section class="php-print">
+        <h2> Car List</h2>
+        <ul>
+            @foreach ($cars as $car)
+                <li>
+                    <h3>{{ $car->modello }}</h3>
+                    <div class="brand">{{ $car->marca }}</div>
+                    <div class="plate">{{ $car->targa }}</div>
+                    {{-- Loop variable --}}
+                    @if($loop->last)
+                        <hr>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
+    </section>
 
-    <h2> Car List</h2>
-
-    <ul>
-        @foreach ($cars as $car)
-            <li>
-                <h3>{{ $car->modello }}</h3>
-                <div class="brand">{{ $car->marca }}</div>
-                <div class="plate">{{ $car->targa }}</div>
-                {{-- Loop variable --}}
-                @if($loop->last)
-                    <hr>
-                @endif
+    <section id="app">
+        <h2> Bikes List App</h2>
+        <ul>
+            <li v-for="bike in bikes">
+                <h3> @{{ bike.marca }}</h3>
+                <div class="brand">@{{ bike.modello}}</div>
+                <div class="plate"> @{{bike.targa}}</div>
             </li>
-        @endforeach
-    </ul>
-
+        </ul>
+    </section>
+    {{-- JS --}}
+    <script src="{{asset('js/app.js')}}"></script>
 @endsection
