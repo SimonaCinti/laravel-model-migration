5f8914ac8f8b386a447341cc9c81dfd6db16d347
Simona Cinti <simona.cinti2@gmail.com>
2021-01-20 16:21:17 +0100

    browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+    })
+    .browserSync({
+        proxy: 'http://127.0.0.1:8000',
+        watch: true,
+        files: ['./**/*.css', './**/*.js', './**/*.+(html|php)'],
+        //tunnel: true, //for mobile
+    });
